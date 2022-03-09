accc46e3f22cb0c00e72597ba84dc71fc6eba7e6
Roni Palva-aho <roni.palva-aho@pinja.com>
2022-03-09 15:15:24 +0200

    Upgrade agent to 1.1

--- edge-provision.sh
+++ edge-provision.sh
@@ -26,7 +26,7 @@ agent:
   type: "docker"
   env: {}
   config:
-    image: "mcr.microsoft.com/azureiotedge-agent:1.0"
+    image: "mcr.microsoft.com/azureiotedge-agent:1.1"
     auth: {}
 hostname: $(cat /proc/sys/kernel/hostname)
 connect:
